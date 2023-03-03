2e7776b00f4fe73664f9bf1e7552859b673ead61
Miguel Diaz <diazmiiguel@gmail.com>
2023-03-02 20:59:15 -0500

    init :rocket:

---
+++ README.md
@@ -0,0 +1,12 @@
+The name is a bit weird but I found it funny because it is centered around housing.
+
+What I want to do with this is run a few things in my TV and sound system at my home. I stopped using Chromium based browsers so it's a pain in the ass to
+stream things from my computer's youtube to my tv. I ultimately want this to work with my Google Nest too but for now it is good since it can play youtube
+videos through the WS that my TV exposes in my local network.
+
+TODO:
+
+- Do not hardcode the TV IP and issue an M-SEARCH (?) to find it in the local network
+- Accept commands while the websocket client is running
+- Include a search for YouTube videos, at the moment I can only paste whole youtube links
+- The initial HELLO payload has a client-key hardcoded in there to avoid my TV's request for pairing every time. I will be moving this to SQLite
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use clap::Parser;
+use serde::Deserialize;
+use serde_json::{json, Value};
+use std::net::TcpStream;
+use tungstenite::protocol::WebSocket;
+use tungstenite::stream::MaybeTlsStream;
+use tungstenite::{connect, Message};
+use url::Url;
+
+#[derive(Deserialize, Debug)]
+struct Response {
+    #[serde(rename = "type")]
+    command_type: String,
+}
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    yt_link: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let (mut socket, _response) =
+        connect(Url::parse("ws://192.168.1.6:3000").unwrap()).expect("Cannot connect");
+
+    println!("Connected");
+
+    socket.write_message(Message::Text(HELLO.into())).unwrap();
+
+    loop {
+        let raw_msg = socket.read_message().expect("Error reading message");
+
+        if let Ok(msg) = serde_json::from_str(raw_msg.to_text().unwrap()) {
+            handle_message(msg, &mut socket, &args);
+        }
+    }
+}
+
+fn handle_message(msg: Response, socket: &mut WebSocket<MaybeTlsStream<TcpStream>>, args: &Args) {
+    match msg.command_type.as_str() {
+        "registered" => open_yt(socket, args),
+
+        _ => println!("Received {:?}", msg),
+    }
+}
+
+fn open_yt(socket: &mut WebSocket<MaybeTlsStream<TcpStream>>, args: &Args) {
+    let payload: Value = json!({
+        "type": "request",
+        "id": "youtube_1",
+        "uri": "ssap://system.launcher/launch",
+        "payload": {
+            "id": "youtube.leanback.v4",
+            "params": {
+                "contentTarget": args.yt_link
+            }
+        }
+    });
+
+    socket
+        .write_message(Message::Text(payload.to_string()))
+        .unwrap();
+}
+
+static HELLO: &str = r##"
+{"id":"register_0","payload":{"client-key":"36aba158f01f5c09ce3eb658a61c65cc","forcePairing":false,"manifest":{"appVersion":"1.1","manifestVersion":1,"permissions":["LAUNCH","LAUNCH_WEBAPP","APP_TO_APP","CLOSE","TEST_OPEN","TEST_PROTECTED","CONTROL_AUDIO","CONTROL_DISPLAY","CONTROL_INPUT_JOYSTICK","CONTROL_INPUT_MEDIA_RECORDING","CONTROL_INPUT_MEDIA_PLAYBACK","CONTROL_INPUT_TV","CONTROL_POWER","READ_APP_STATUS","READ_CURRENT_CHANNEL","READ_INPUT_DEVICE_LIST","READ_NETWORK_STATE","READ_RUNNING_APPS","READ_TV_CHANNEL_LIST","WRITE_NOTIFICATION_TOAST","READ_POWER_STATE","READ_COUNTRY_INFO","READ_SETTINGS","CONTROL_TV_SCREEN","CONTROL_TV_STANBY","CONTROL_FAVORITE_GROUP","CONTROL_USER_INFO","CHECK_BLUETOOTH_DEVICE","CONTROL_BLUETOOTH","CONTROL_TIMER_INFO","STB_INTERNAL_CONNECTION","CONTROL_RECORDING","READ_RECORDING_STATE","WRITE_RECORDING_LIST","READ_RECORDING_LIST","READ_RECORDING_SCHEDULE","WRITE_RECORDING_SCHEDULE","READ_STORAGE_DEVICE_LIST","READ_TV_PROGRAM_INFO","CONTROL_BOX_CHANNEL","READ_TV_ACR_AUTH_TOKEN","READ_TV_CONTENT_STATE","READ_TV_CURRENT_TIME","ADD_LAUNCHER_CHANNEL","SET_CHANNEL_SKIP","RELEASE_CHANNEL_SKIP","CONTROL_CHANNEL_BLOCK","DELETE_SELECT_CHANNEL","CONTROL_CHANNEL_GROUP","SCAN_TV_CHANNELS","CONTROL_TV_POWER","CONTROL_WOL"],"signatures":[{"signature":"eyJhbGdvcml0aG0iOiJSU0EtU0hBMjU2Iiwia2V5SWQiOiJ0ZXN0LXNpZ25pbmctY2VydCIsInNpZ25hdHVyZVZlcnNpb24iOjF9.hrVRgjCwXVvE2OOSpDZ58hR+59aFNwYDyjQgKk3auukd7pcegmE2CzPCa0bJ0ZsRAcKkCTJrWo5iDzNhMBWRyaMOv5zWSrthlf7G128qvIlpMT0YNY+n/FaOHE73uLrS/g7swl3/qH/BGFG2Hu4RlL48eb3lLKqTt2xKHdCs6Cd4RMfJPYnzgvI4BNrFUKsjkcu+WD4OO2A27Pq1n50cMchmcaXadJhGrOqH5YmHdOCj5NSHzJYrsW0HPlpuAx/ECMeIZYDh6RMqaFM2DXzdKX9NmmyqzJ3o/0lkk/N97gfVRLW5hA29yeAwaCViZNCP8iC9aO0q9fQojoa7NQnAtw==","signatureVersion":1}],"signed":{"appId":"com.lge.test","created":"20140509","localizedAppNames":{"":"LG Remote App","ko-KR":"리모컨 앱","zxx-XX":"ЛГ Rэмotэ AПП"},"localizedVendorNames":{"":"LG Electronics"},"permissions":["TEST_SECURE","CONTROL_INPUT_TEXT","CONTROL_MOUSE_AND_KEYBOARD","READ_INSTALLED_APPS","READ_LGE_SDX","READ_NOTIFICATIONS","SEARCH","WRITE_SETTINGS","WRITE_NOTIFICATION_ALERT","CONTROL_POWER","READ_CURRENT_CHANNEL","READ_RUNNING_APPS","READ_UPDATE_INFO","UPDATE_FROM_REMOTE_APP","READ_LGE_TV_INPUT_EVENTS","READ_TV_CURRENT_TIME"],"serial":"2f930e2d2cfe083771f68e4fe7bb07","vendorId":"com.lge"}},"pairingType":"PROMPT"},"type":"register"}
+"##;
